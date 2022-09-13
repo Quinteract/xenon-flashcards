ea23f61578f89ba3efe6879361aa7a29b031f133
Quinteract <76565848+Quinteract@users.noreply.github.com>
2022-09-13 13:21:52 +0000

    Added answer buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,48 @@
-<h1>Xenon Flashcards</h1>
+<div id="navbar">
+    Xenon Flashcards
+</div>
+<div id="card"></div>
+<div id="answerbar">
+    <div id="answerbuttons">
+        <button id="wrong">Wrong</button>
+        <button id="right">Right</button>
+    </div>
+</div>
+
+<style>
+* {
+    font-family: sans-serif;
+}
+
+#answerbar {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 50px;
+    text-align: center;
+    border-top: 2px solid black;
+}
+
+#answerbuttons {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+}
+
+#wrong, #right {
+    margin: 0px 20px;
+    padding: 5px;
+    font-size: 20px;
+    border: 2px solid black;
+    border-radius: 4px;
+}
+
+#wrong {
+    background-color: firebrick;
+}
+
+#right {
+    background-color: olivedrab;
+}
+</style>
