b5fb19515ff20d3e4ee047cc8c9180b2ff6b5c39
Quinteract <76565848+Quinteract@users.noreply.github.com>
2022-09-15 13:51:48 +0000

    changed to static site

---
+++ src/routes/edit/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { page } from '$app/stores';
+    
+    var card = $page.url.searchParams.has('card') ? $page.url.searchParams.get('card') : "";
+</script>
+
+<div id="browser">
+    <input id="query">
+</div>
+<div id="editor">
+    {card}
+</div>
